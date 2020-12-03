6ff10464656557935d8d0e91a641b62284b54878
Apriyanto J.P.L Tobing <apriyantotobing8@gmail.com>
2020-12-03 16:45:10 +0700

    feat: learn Component Syntax
    Component Syntax; slot & props, flow data from parent to child, styling component scoped

---
+++ scrimba/09 - component-syntax/05 - to-do-list-app/components/List.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul>
+        <li v-for="item in items" :key="item">{{ item }}</li>
+    </ul>
+</template>
+
+<script>
+    module.exports = {
+        props: ['items']
+    }
+</script>
+
+<style></style>
---
+++ scrimba/09 - component-syntax/components/ExampleOne.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="wrapper">
+        <a href="#">Link</a>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .wrapper {
+        background: #ccc;
+        margin: 15px;
+    }
+    a {
+        font-size: 100px;
+    }
+</style>
