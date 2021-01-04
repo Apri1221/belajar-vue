5e64ecf351ec465e13c0dda343c81a30542dd905
Apriyanto J.P.L Tobing <apriyanto.tobing@adira.co.id>
2021-01-04 21:52:12 +0700

    refactor: update props, emit, and required props

---
+++ scrimba/09 - component-syntax/06 - advanced-props/components/Props.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Advanced Props</h1>
+        
+        <div>{{ isPublished }}</div>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        props: {
+            // ini tuh pengganti type, jadi sama aja kaya id: {type: Number}
+            id: Number,
+            title: String,
+            isPublished: {
+                // ini bisa acc 2 jenis tipe data
+                type: [Boolean, Number],
+                // kalau misalnya nanti props isPublished tidak didefenisikan, maka isPublished bernilai false (kalau dibuat atribut default)
+                default: false
+            },
+            views: Number,
+        }
+    }
+</script>
+
+<style>
+    
+</style>
